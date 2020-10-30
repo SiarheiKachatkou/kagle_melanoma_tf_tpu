767f8ac0e2d34644c97407184bca86e533e59ec2
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-10-30 17:08:57 +0300

    do_not_use_metrics

--- consts.py
+++ consts.py
@@ -40,7 +40,7 @@ CONFIG=config(lr_max=0.0002*8, lr_start=0.0002*8, lr_warm_up_epochs=0, lr_min=0.
               l2_penalty=1e-6, work_dir=work_dir_name,
               gs_work_dir=f'gs://kochetkov_kaggle_melanoma/{work_dir_name}_{str(datetime.datetime.now())}',
               model_fn_str="efficientnet.tfkeras.EfficientNetB4(weights='imagenet', include_top=False)", ttas=1,
-              use_metrics=True
+              use_metrics=False
               )
 
 #pretrained_model = tf.keras.applications.MobileNetV2(input_shape=[*IMAGE_SIZE, 3], include_top=False)
