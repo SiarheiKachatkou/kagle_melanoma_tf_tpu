94a6cc084344c6f4a35ac5da1fed6663ed19527e
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-03 15:50:53 +0300

    nofine_tune_test

--- consts.py
+++ consts.py
@@ -9,7 +9,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 
 is_debug = False
 
-EPOCHS_FINE_TUNE = 1
+EPOCHS_FINE_TUNE = 0
 EPOCHS_FULL = 2 if is_debug else 10
 
 IMAGE_HEIGHT = 768
@@ -44,10 +44,10 @@ TRAIN_STEPS = 1 if is_debug else 50000//BATCH_SIZE
 
 config=namedtuple('config',['lr_max','lr_start','lr_warm_up_epochs','lr_min','lr_exp_decay','nfolds','l2_penalty','model_fn_str','work_dir', 'gs_work_dir','ttas','use_metrics'])
 
-work_dir_name='b4_focal_loss_768_old_datasets_fine_tune'
+work_dir_name = 'b4_focal_loss_768_old_datasets' #'b4_focal_loss_768_old_datasets_fine_tune'
 
 
-CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8, nfolds=4,
+CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8, nfolds=2,
               l2_penalty=1e-6, work_dir=work_dir_name,
               gs_work_dir=f'gs://kochetkov_kaggle_melanoma/{work_dir_name}_{str(datetime.datetime.now())}',
               model_fn_str="efficientnet.tfkeras.EfficientNetB4(weights='imagenet', include_top=False)", ttas=1,
