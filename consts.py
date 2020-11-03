f7055a9691cd32cbec51df21f03552a9e5d7b855
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-03 08:06:31 +0300

    red_8

--- consts.py
+++ consts.py
@@ -23,7 +23,9 @@ DATASETS = {
 
 CLASSES = ['health','melanoma']
 
-BATCH_SIZE = 2 if is_debug else 2*8 #4*8*2 #2*8
+red = 8
+
+BATCH_SIZE = 2 if is_debug else 4*8*2//red
 
 # 4*8*2 = 64- 18 min/epoch (time from TPU monitor)
 # 6*8=48 - 27 min/epoch (time from TPU monitor)
@@ -43,7 +45,7 @@ config=namedtuple('config',['lr_max','lr_start','lr_warm_up_epochs','lr_min','lr
 
 work_dir_name='b7_focal_loss_768_old_datasets'
 
-red=1
+
 CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8, nfolds=4,
               l2_penalty=1e-6, work_dir=work_dir_name,
               gs_work_dir=f'gs://kochetkov_kaggle_melanoma/{work_dir_name}_{str(datetime.datetime.now())}',
