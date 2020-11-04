ad06a4ac059316bb0284bf14d26b0c95373fc906
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-04 06:58:54 +0300

    tpu2

--- run_tpu.sh
+++ run_tpu.sh
@@ -1,3 +1,6 @@
+
+TPU=${1:-3}
+
 export BUCKET_NAME=kochetkov_belwest
 export PROJECT_ID=turing-audio-146210
 gcloud config set project $PROJECT_ID
@@ -5,6 +8,6 @@ gcloud config set project $PROJECT_ID
 ctpu up --project=${PROJECT_ID} \
  --zone=us-central1-b \
  --tf-version=2.3.1 \
- --tpu-size=v3-8 \
- --name=tpu-melanoma \
+ --tpu-size=v${TPU}-8 \
+ --name=tpu-melanoma-${TPU} \
  --preemptible
