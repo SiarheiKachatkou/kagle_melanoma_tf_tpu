67e3ddf93432d440f631fc292bb66b7314428330
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-05 08:30:51 +0300

    1e3_penalty

--- consts.py
+++ consts.py
@@ -8,7 +8,7 @@ use_tpu_2 = False
 tpu3='grpc://10.240.1.2:8470'
 
 tpu2="grpc://10.240.1.10:8470"
-#os.environ['TPU_NAME']=tpu2 if use_tpu_2 else tpu3
+os.environ['TPU_NAME']=tpu2 if use_tpu_2 else tpu3
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 
@@ -38,7 +38,7 @@ TRAIN_STEPS = 1 if is_debug else 50000//BATCH_SIZE
 config=namedtuple('config',['lr_max','lr_start','lr_warm_up_epochs','lr_min','lr_exp_decay','nfolds','l2_penalty',
                             'model_fn_str','work_dir', 'gs_work_dir','ttas','use_metrics','dropout_rate'])
 
-work_dir_name = 'b4_focal_loss_768_old_datasets_penalty_1e-3'
+work_dir_name = 'b4_focal_loss_768_old_datasets_penalty_1e-6'
 
 
 CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8,
