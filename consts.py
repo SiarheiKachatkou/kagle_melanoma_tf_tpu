f83084a8cefb0af8154a20d12a5f83943237869d
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-05 07:42:45 +0300

    1e3_penalty

--- consts.py
+++ consts.py
@@ -14,7 +14,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 
 is_debug = False
 
-EPOCHS_FINE_TUNE = 3
+EPOCHS_FINE_TUNE = 0
 EPOCHS_FULL = 2 if is_debug else 20
 
 IMAGE_HEIGHT = 768
@@ -38,11 +38,11 @@ TRAIN_STEPS = 1 if is_debug else 50000//BATCH_SIZE
 config=namedtuple('config',['lr_max','lr_start','lr_warm_up_epochs','lr_min','lr_exp_decay','nfolds','l2_penalty',
                             'model_fn_str','work_dir', 'gs_work_dir','ttas','use_metrics','dropout_rate'])
 
-work_dir_name = 'b4_focal_loss_768_old_datasets_large_penalty'
+work_dir_name = 'b4_focal_loss_768_old_datasets_penalty_1e-3'
 
 
 CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8,
-              nfolds=2, l2_penalty=1e+3, work_dir=work_dir_name,
+              nfolds=2, l2_penalty=1e-3, work_dir=work_dir_name,
               gs_work_dir=f'gs://kochetkov_kaggle_melanoma/{work_dir_name}_{str(datetime.datetime.now())}',
               model_fn_str="efficientnet.tfkeras.EfficientNetB4(weights='imagenet', include_top=False)", ttas=1,
               use_metrics=True, dropout_rate=0.5
