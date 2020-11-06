6597ca5157eec693059cb38dff95650f4c8b3f70
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-06 19:55:51 +0300

    b6_for_tpu_2

--- run_tpu.sh
+++ run_tpu.sh
@@ -9,13 +9,14 @@ ctpu up --project=${PROJECT_ID} \
  --zone=us-central1-b \
  --tf-version=2.3.1 \
  --tpu-size=v${TPU}-8 \
- --name=tpu-melanoma-${TPU} \
- --preemptible
+ --name=tpu-melanoma-${TPU} #\
+ #--preemptible
 
 exit 0
 
 screen
-git clone https://github.com/SiarheiKachatkou/kagle_melanoma_tf_tpu.git
-cd kagle_melanoma_tf_tpu
-./install.sh
+
+git clone https://github.com/SiarheiKachatkou/kagle_melanoma_tf_tpu.git &&
+cd kagle_melanoma_tf_tpu &&
+./install.sh &&
 python3 main.py
