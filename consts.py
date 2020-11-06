36c8e34ba4e1c624b5ffcb7451c853bfdf6bf475
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-06 07:16:19 +0300

    1e-12

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 import os
 
 
-use_tpu_2 = False
+use_tpu_2 = True
 
 tpu3 = 'grpc://10.240.1.2:8470'
 tpu2 = "grpc://10.240.1.10:8470"
@@ -40,11 +40,11 @@ TRAIN_STEPS = 1 if is_debug else 50000//BATCH_SIZE
 config=namedtuple('config',['lr_max','lr_start','lr_warm_up_epochs','lr_min','lr_exp_decay','nfolds','l2_penalty',
                             'model_fn_str','work_dir', 'gs_work_dir','ttas','use_metrics','dropout_rate'])
 
-work_dir_name = 'b4_focal_loss_768_old_datasets_penalty_1e-6'
+work_dir_name = 'b4_focal_loss_768_old_datasets_penalty_1e-9'
 
 
 CONFIG=config(lr_max=0.0002*8/red, lr_start=0.0002*8/red, lr_warm_up_epochs=0, lr_min=0.000005/red,lr_exp_decay=0.8,
-              nfolds=4, l2_penalty=1e-6, work_dir=work_dir_name,
+              nfolds=4, l2_penalty=1e-9, work_dir=work_dir_name,
               gs_work_dir=f'gs://kochetkov_kaggle_melanoma/{work_dir_name}_{str(datetime.datetime.now())}',
               model_fn_str="efficientnet.tfkeras.EfficientNetB4(weights='imagenet', include_top=False)", ttas=1,
               use_metrics=True, dropout_rate=0.5
