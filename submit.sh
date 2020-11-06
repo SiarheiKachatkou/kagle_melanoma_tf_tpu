2f8aeea21fad69c42ee447235f7e3990ea8a72b5
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-06 18:34:26 +0300

    b6

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 
 set -x
 
-gs_folders=( "b4_focal_loss_768_old_datasets_penalty_1e-6_2020-11-06 04:42:40.388071" "b4_focal_loss_768_old_datasets_penalty_1e-9_2020-11-06 04:27:19.296397" )
+gs_folders=( "b4_focal_loss_768_old_datasets_penalty_1e-9_2020-11-06 04:27:19.296397/b4_focal_loss_768_old_datasets_penalty_1e-9" )
 dst_folder=artifacts
 
 for folder in "${gs_folders[@]}"
