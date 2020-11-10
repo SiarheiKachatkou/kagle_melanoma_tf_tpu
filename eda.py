c9fa79c02bc43bf4cb09840fca6e18d35a62c9cc
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-10 12:50:15 +0300

    gitignor

---
+++ eda.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path='data/256x256'
+
+train_info=pd.read_csv(os.path.join(data_path,'train.csv'))
+
+test_info=pd.read_csv(os.path.join(data_path,'test.csv'))
+
+print(train_info.head())
+
+print(test_info.head())
+
+
+print(f'train missed values {train_info.isnull().sum()}')
+
+print(f'test missed values {test_info.isnull().sum()}')
+
+dbg=1
+
+print('---- patient ids----')
+
+train_info.patient_id.value_counts().plot()
+plt.title('train patience id counts')
+plt.show()
+
+test_info.patient_id.value_counts().plot()
+plt.title('test patience id counts')
+plt.show()
+
+test_ids=set(test_info.patient_id)
+train_ids=set(train_info.patient_id)
+print(f'patiens in test present in train {test_ids.intersection(train_ids)}')
+
+print('---- age----')
+
+train_info.age_approx.hist(bins=20)
+plt.title('train age')
+plt.show()
+
+test_info.age_approx.hist(bins=20)
+plt.title('test age')
+plt.show()
