bbf9d5c992c20c5f9f0f2e3860137136bba02153
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-16 19:46:24 +0300

    batch_size

--- consts.py
+++ consts.py
@@ -48,7 +48,7 @@ CLASSES = ['health','melanoma']
 
 red = 4 if use_tpu_2 else 1
 
-BATCH_SIZE = 1 if is_debug else 32//red
+BATCH_SIZE = 1 if is_debug else 8*32//red
 
 TRAIN_STEPS = 1 if is_debug else None
 
