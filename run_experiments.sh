6fe2ec214bdc429717529901c54a9f44eed73c2f
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-24 09:39:26 +0300

    refactor

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-for backbone in B0 B1 B2
+for backbone in B0 B1
 do
-  for cut_mix_prob in 0 0.1
+  for cut_mix_prob in 0.1 0
    do
-     for dropout_rate in 0 0.1 0.5
+     for dropout_rate in 0  0.1 0.5
      do
-       python main.py --backbone=$backbone --cut-mix-prob=$cut_mix_prob --dropout-rate=$dropout_rate
+       python train_and_test.py --backbone=$backbone --cut-mix-prob=$cut_mix_prob --dropout-rate=$dropout_rate
      done
    done
 done
