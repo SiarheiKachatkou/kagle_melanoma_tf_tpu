5ac752963ea6dec65b90365e3a1b23974330d0fe
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-28 07:26:51 +0300

    focal_loss_params_through_arguments

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,13 +4,16 @@ for backbone in B0 B1
 do
     for dropout_rate in  0 0.05
      do
-       for lr_max in 1 10
+       for lr_max in 10 100
        do
          for lr_exp_decay in 0.8 0.5
           do
-            for oversample_mult in 1 2 3
+            for focal_loss_gamma in 0.25 1 2
             do
-              python train_and_test.py --backbone=$backbone --oversample_mult=$oversample_mult --dropout-rate=$dropout_rate --lr_max=$lr_max --lr_exp_decay=$lr_exp_decay
+              for focal_loss_alpha in 0.5 0.75 0.8
+              do
+                python train_and_test.py --backbone=$backbone --dropout-rate=$dropout_rate --lr_max=$lr_max --lr_exp_decay=$lr_exp_decay --focal_loss_gamma=$focal_loss_gamma --focal_loss_alpha=$focal_loss_alpha
+              done
             done
           done
         done
