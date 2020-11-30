182c1f8f40fbb10e68c449a60641cd2f0353c9b8
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-30 07:57:40 +0300

    augm_data_from_kaggle_env

--- augmentation_microscope.py
+++ augmentation_microscope.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from consts import IMAGE_HEIGHT
+from consts import IMAGE_HEIGHT, is_local
 from augmentations_geom import transform_geometricaly
 
-GCS_PATH_microscope_images = "data/malanoma_microscope" # "gs://kochetkov_kaggle_melanoma/malanoma_hairs"
+GCS_PATH_microscope_images = "data/melanoma_microscope" if is_local else "/kaggle/input/melanoma_microscope"
 microscope_images = tf.io.gfile.glob(GCS_PATH_microscope_images + '/*.png')
 microscope_images_tf=tf.convert_to_tensor(microscope_images)
 
