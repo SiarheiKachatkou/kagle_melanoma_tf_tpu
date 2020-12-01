685ab71f3439041b76e6524764f807d9b0ed9c44
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-01 05:00:46 +0300

    fix_kaggle_segmentation_fault

--- dataset_utils.py
+++ dataset_utils.py
@@ -137,10 +137,10 @@ def get_training_dataset(training_fileimages, training_fileimages_old, config, r
     dataset = dataset.repeat(repeats)
     dataset=dataset.shuffle(512)
     #dataset=oversample(dataset,config)
-    #augm_fn=partial(augment_train,config=config)
-    #dataset = _augm_dataset(dataset,augm_fn)
-    dataset = dataset.batch(BATCH_SIZE)
-    dataset = dataset.prefetch(AUTO)
+    augm_fn=partial(augment_train,config=config)
+    dataset = _augm_dataset(dataset,augm_fn)
+    #dataset = dataset.batch(BATCH_SIZE)
+    #dataset = dataset.prefetch(AUTO)
 
     return dataset
 
