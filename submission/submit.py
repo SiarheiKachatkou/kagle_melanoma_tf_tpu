fefa4666a76fa7498775df2f20b7d6d1bf771210
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-09 09:19:06 +0300

    unchange_under_averagin_tests_passed

--- submission/submit.py
+++ submission/submit.py
@@ -31,6 +31,7 @@ def main():
             print(f'{le}_val_single_model_{m_type}metrics={aucs}')
             print(f'{le}_val_single_model_{m_type}avg_metric={np.mean(aucs)}')
 
+    avg_test_subms=[]
     for le in ['', 'le']:
         for m_type in ['', 'tta_']:
             aucs = []
@@ -45,6 +46,7 @@ def main():
                     subs.append(sub)
             if subs:
                 avg_sub = avg_submissions(subs)
+                avg_test_subms.append(avg_sub)
                 auc_avg_sub = calc_auc(avg_sub)
                 save_submission(avg_sub, os.path.join(args.work_dir, 'kaggle_' + f'test_{le}_{m_type}.csv'))
             else:
@@ -54,7 +56,7 @@ def main():
             print(f'{le}_test_single_model_{m_type}avg_metric={np.mean(aucs)}')
             print(f'{le}_test_avg_model_{m_type}_metric={auc_avg_sub}')
 
-    return aa
+    return avg_test_subms
 
 
 if __name__=="__main__":
--- submission/submit_test.py
+++ submission/submit_test.py
@@ -1,17 +1,35 @@
 import os
 import argparse
 import unittest
+import random
 import pandas as pd
 from unittest.mock import  patch
 from config.consts import ARTIFACTS_ROOT
 from submission.submit import main
 
+COL_NAMES=['image_name', 'target', 'labels']
+DATA=[['img1',1,1],['img2',2,0],['img3',3,1],['img4',4,0],['img5',5,1]]
+
 def read_scv_synth(filename):
-    print(filename)
-    col_names=['image_name','target','labels']
-    df=pd.DataFrame(data=[['img1',1,1],['img2',2,0],['img3',3,1],['img4',4,0],['img5',5,1]],
-                              columns=col_names)
+    #print(filename)
+
+    data=DATA
+    random.shuffle(data)
+    df=pd.DataFrame(data=data, columns=COL_NAMES)
     return df
+
+
+def does_df_changed(df_list):
+    etalon_df=pd.DataFrame(data=DATA,columns=COL_NAMES)
+    etalon_df=etalon_df.sort_values(by='image_name')
+    for df in df_list:
+        df=df.sort_values(by='image_name')
+        for key in COL_NAMES:
+            if not all(etalon_df[key].values==df[key].values):
+                return True
+    return False
+
+
 
 class SubmitTTATest(unittest.TestCase):
 
@@ -20,20 +38,24 @@ class SubmitTTATest(unittest.TestCase):
     @patch('submission.submission_utils.pd.DataFrame.to_csv')
     @patch('submission.parseargs')
     def test_tta_submit(self, mock_parse_args, mock_to_csv, mock_read_csv, mock_exists):
+        random.seed(0)
+        nfolds=3
         args=argparse.Namespace()
-        args.folds=4
+        args.folds=nfolds
         args.work_dir=os.path.join(ARTIFACTS_ROOT,'tmp')
         mock_parse_args.parse_args.return_value=args
 
         mock_to_csv.return_value=True
 
         mock_read_csv.read_csv.side_effect = read_scv_synth
-        mock_exists.exists.return_value=True
+        mock_exists.return_value=True
 
 
-        main()
+        avg_test_subms = main()
 
-        mock_read_csv.assert_called()
+        self.assertEqual(mock_read_csv.read_csv.call_count,nfolds*2*2*2)
+
+        self.assertTrue(not does_df_changed(avg_test_subms))
 
 
 
