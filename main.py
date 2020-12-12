cf6b239cfc80a109b848fc9b13aac88096ffcb79
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-12 06:19:25 +0300

    dbg

--- main.py
+++ main.py
@@ -52,10 +52,9 @@ for i,k in enumerate(IMG_SIZES):
     GCS_PATH[i] = 'gs://kds-68d604aee5addfebbabae3fcfe5376b86711f3cd42993bd8e0dc80a5'
     GCS_PATH2[i] = 'gs://kds-4e8502fa6aa4c08b11f43ab8b42505960a29dc73fbcea54ba2bd1f9a'
 files_train = np.sort(np.array(tf.io.gfile.glob(GCS_PATH[0] + '/train*.tfrec')))
-print(f'files_train={files_train}')
+print(f'files_train_count={len(files_train)}')
 files_test  = np.sort(np.array(tf.io.gfile.glob(GCS_PATH[0] + '/test*.tfrec')))
 
-assert False
 
 if DEVICE == "TPU":
     print("connecting to TPU...")
