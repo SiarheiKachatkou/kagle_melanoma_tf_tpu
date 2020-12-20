5c212d36dcf94358c0def24cf4331dfe63f44352
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-20 10:25:43 +0300

    tpu_fix

--- config/config.py
+++ config/config.py
@@ -31,8 +31,8 @@ if is_local:
         os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
 
 
-epochs_fine_tune = 20
-epochs_full = 1 if is_debug else 40
+epochs_fine_tune = 30
+epochs_full = 1 if is_debug else 50
 
 
 BATCH_SIZE = 36 if is_debug else 512
