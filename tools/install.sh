860596f30a7a6494847437b630a144ef51815d22
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-27 09:42:14 +0300

    install_script_fix

--- tools/install.sh
+++ tools/install.sh
@@ -9,6 +9,6 @@ pip3 install dvc
 pip3 install 'dvc[gdrive]'
 sudo apt install libgl1-mesa-glx -y
 pip3 install keras==2.3.0
-pip3 uninstall keras-applications
+pip3 uninstall keras-applications -y
 pip3 install -r tools/requirements.txt
 #pip3 install tensorflow-addons
