c0d10750b2e191b467c35849ef6c45b18ea15b83
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-12-28 15:22:11 +0300

    fix

--- tools/avg_submits_from_dvc.sh
+++ tools/avg_submits_from_dvc.sh
@@ -2,7 +2,7 @@
 TAGS=( baseline_b6 best_B0_384_baseline B6_with_meta B0_384_with_meta)
 for t in ${TAGS[@]}
 do
-  git checkout $t
-  dvc pull
+  git checkout $t &&
+  dvc pull &&
   cp artifacts/baseline/kaggle_test__tta_.csv ../subms/$t.csv
 done
