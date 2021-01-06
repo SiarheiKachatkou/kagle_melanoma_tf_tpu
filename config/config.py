bdf286981f934ff1e5540810b8fe65848ea01f1f
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-01-06 05:57:59 +0300

    more_epochs_strong_penalty

--- config/config.py
+++ config/config.py
@@ -34,10 +34,10 @@ if is_local:
 
 
 epochs_fine_tune = 0
-epochs_full = 1 if is_debug else epochs_fine_tune+12
+epochs_full = 1 if is_debug else epochs_fine_tune+20
 
 
-BATCH_SIZE = 36 if is_debug else args.batch_size#400
+BATCH_SIZE = 36 if is_debug else args.batch_size
 BATCH_SIZE_INCREASE_FOR_INFERENCE = 4
 
 
