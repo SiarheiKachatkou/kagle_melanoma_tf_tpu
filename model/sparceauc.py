63bda540ff4799a7a3a7ec1faa9b778cc3890ae6
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-01-08 10:02:14 +0300

    prod_config_auc_real

--- model/sparceauc.py
+++ model/sparceauc.py
@@ -7,10 +7,8 @@ class SparceAUC(tf.keras.metrics.AUC):
 
 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        y_true_one_hot = tf.compat.v1.one_hot(tf.cast(y_true, dtype=tf.int32), depth=2)
-        y_true_one_hot = tf.keras.backend.squeeze(y_true_one_hot, axis=1)
 
-        super().update_state(y_true_one_hot, y_pred, sample_weight)
+        super().update_state(y_true, y_pred[:,1], sample_weight)
 
     def result(self):
         return super().result()
