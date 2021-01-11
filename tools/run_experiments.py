afd285c611950ed24f3d12fc5973cf7e7238d02f
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-01-11 07:26:38 +0300

    fix

--- tools/run_experiments.py
+++ tools/run_experiments.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from itertools import product
 from config.consts import is_local
 
-hparams={'backbone':['B0','B1'], 'dropout_rate':[0.005,0.001], 'lr_max':[1, 0.1, 0.5], 'lr_exp_decay':[0.8,0.5],'hair_prob':[0,0.05, 0.1],'microscope_prob':[0,0.01],'lr_warm_up_epochs':[2,6,8],'image_height':[128,128], 'batch_size':[64], 'save_best_n':[1,2,4]}
+hparams={'backbone':['B0','B1'], 'dropout_rate':[0.005,0.001], 'lr_max':[1, 0.1, 0.5], 'lr_exp_decay':[0.8,0.5],'hair_prob':[0,0.05, 0.1],'microscope_prob':[0,0.01],'lr_warm_up_epochs':[2,6,8],'image_height':[256], 'batch_size':[64], 'save_best_n':[1,2,4]}
 
 keys=list(hparams.keys())
 val_list=[hparams[k] for k in keys]
