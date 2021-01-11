86b028e472fff2996c19e514550d1743a0d5acee
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-01-11 07:01:58 +0300

    tpu_config

--- tools/run_experiments.py
+++ tools/run_experiments.py
@@ -3,8 +3,9 @@ import random
 import multiprocessing as mp
 from tqdm import tqdm
 from itertools import product
+from config.consts import is_local
 
-hparams={'backbone':['B0','B1'], 'dropout_rate':[0.005,0.001], 'lr_max':[1, 0.1, 0.5], 'lr_exp_decay':[0.8,0.5],'hair_prob':[0,0.05, 0.1],'microscope_prob':[0,0.01],'lr_warm_up_epochs':[2,6,8],'image_height':[128,256], 'batch_size':[64], 'save_best_n':[1,2,4]}
+hparams={'backbone':['B0','B1'], 'dropout_rate':[0.005,0.001], 'lr_max':[1, 0.1, 0.5], 'lr_exp_decay':[0.8,0.5],'hair_prob':[0,0.05, 0.1],'microscope_prob':[0,0.01],'lr_warm_up_epochs':[2,6,8],'image_height':[128,128], 'batch_size':[64], 'save_best_n':[1,2,4]}
 
 keys=list(hparams.keys())
 val_list=[hparams[k] for k in keys]
@@ -30,8 +31,12 @@ def job(input_tuple):
 
     os.system(cmd_string)
 
-num_procs=3
-pool=mp.Pool(num_procs)
-#for _ in tqdm(map(job, enumerate(args)), total=len(args)):
-for _ in tqdm(pool.imap_unordered(job, enumerate(args)), total=len(args)):
+if is_local:
+    num_procs=3
+    pool=mp.Pool(num_procs)
+    map_fn=pool.imap_unordered
+else:
+    map_fn=map
+
+for _ in tqdm(map_fn(job, enumerate(args)), total=len(args)):
     pass
