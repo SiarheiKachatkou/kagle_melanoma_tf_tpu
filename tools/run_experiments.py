3cb4c7623ecbc9d2c545469cea9181b6596c4bf5
sergeykochetkov <sergeykochetkov@yandex.ru>
2021-01-12 10:22:42 +0300

    do_not_shufle_args

--- tools/run_experiments.py
+++ tools/run_experiments.py
@@ -8,16 +8,15 @@ from config.consts import is_local
 
 hparams={'backbone':['B0'], 'dropout_rate':[0], 'lr_max':[5],
          'lr_exp_decay':[0.8],'hair_prob':[0,0.05],
-         'microscope_prob':[0,0.05],
+         'microscope_prob':[0],
          'lr_warm_up_epochs':[5],
          'image_height':[128], 'batch_size':[128], 'save_best_n':[1],
-         'cut_out_prob':[0,0.15],'cut_mix_prob':[0,0.15]}
+         'cut_out_prob':[0,0.15],'cut_mix_prob':[0,0.05,0.1]}
 
 keys=list(hparams.keys())
 val_list=[hparams[k] for k in keys]
 args=list(product(*val_list))
-random.shuffle(args)
-args=args[:10]
+#args=args[:10]
 
 def get_gpu_available():
     worker_id=mp.current_process().name
