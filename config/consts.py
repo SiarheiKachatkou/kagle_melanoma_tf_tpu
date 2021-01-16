2ea8c607f16eb7ef14b0a48df3860b54b5632e14
root <root@tpu-melanoma-3.us-central1-b.c.turing-audio-146210.internal>
2021-01-16 16:06:48 +0000

    dvc_repro_b0

--- config/consts.py
+++ config/consts.py
@@ -7,8 +7,8 @@ from pathlib import Path
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
-use_tpu_2 = True
-is_local = True
+use_tpu_2 = False
+is_local = False
 is_kaggle = False
 is_debug = False
 do_validate = True
